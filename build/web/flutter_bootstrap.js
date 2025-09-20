1a259a74a9c805a207da9c17929c1fd7ef3d5ffe
Aubuga <augustobublath@gmail.com>
2025-09-20 15:35:24 -0300

    Update web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1200434375"
+    serviceWorkerVersion: "4088229849"
   }
 });
